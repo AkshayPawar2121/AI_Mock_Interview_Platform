e80f116d8e269cc6c746be89716bbfae1f707d2e
Akshay Pawar <akshaypawar8308@gmail.com>
2025-04-20 15:38:18 +0530

    implementd interview session

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,17 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,        // Fix for "Can't resolve 'fs'"
+        path: false,      // Often needed alongside fs
+        os: false         // Sometimes required
+      };
+    }
+    return config;
   }
 };
 
