f131924fd3f0374a44e48993ae506b380541a6e7
Akshay Pawar <akshaypawar8308@gmail.com>
2025-04-20 14:42:50 +0530

    Showed generated interviews

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -43,7 +43,7 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
           </div>
 
           <p className='line-clamp-2 mt-5'>
-            {feedback?.finalAssessment || "You haven't taken any interview yet. Take it5 now to improve your skills"}
+            {feedback?.finalAssessment || "You haven't taken any interview yet. Take it now to improve your skills"}
           </p>
 
         </div>
